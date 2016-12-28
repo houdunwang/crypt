5a16180e6eb5b8ac3e19030c171450d1f1efd1a3
houdunwang <2300071698@qq.com>
2016-12-28 23:41:01 +0800

    no message

---
+++ src/helper.php
@@ -0,0 +1,24 @@
+<?php
+if ( ! function_exists( 'encrypt' ) ) {
+	/**
+	 * 加密
+	 * @param $content
+	 *
+	 * @return mixed
+	 */
+	function encrypt( $content ) {
+		return Crypt::encrypt( $content, 'hdphp.com' );
+	}
+}
+
+if ( ! function_exists( 'decrypt' ) ) {
+	/**
+	 * 解密
+	 * @param $content
+	 *
+	 * @return mixed
+	 */
+	function decrypt( $content ) {
+		return Crypt::decrypt( $content, 'hdphp.com' );
+	}
+}
